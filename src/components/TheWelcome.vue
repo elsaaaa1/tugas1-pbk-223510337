829e683319109e0facda24004a8d79cb30996d65
elsa <elsaputri2038@gmail.com>
2024-04-04 00:51:51 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,55 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Pendidikan</template>
+    Saya SD dipekanbaru, kemudian SMP dan SMA dibatam, lalu kembali lagi kepekanbaru dan masuk universitas islam riau
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Hobi</template>
+
+    Hobi saya bermain basket
+
+    <br />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Tempat Tanggal Lahir</template>
+
+    Pekanbaru, 26 Oktober 2003
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Alamat</template>
+    Pekanbaru
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Tentang Saya</template>
+
+    saya suka bermain basket, mendengarkan musik, menulis, saya anak kedua dari 3 bersaudara.
+  </WelcomeItem>
+</template>
